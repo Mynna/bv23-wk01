52248c7f65cbf8b7ad969e1b44ed0d99e184a20a
Mynna <my2020anna@gmail.com>
2023-04-18 21:18:48 +0200

    noch ohne Kommentar

--- Blatt01_Aufg02.py
+++ Blatt01_Aufg02.py
@@ -1,1 +1,32 @@
-print("Hello")
+
+#Aufgabe 2
+
+#1.
+l='toarmumiknif'
+l=list(l)
+
+#2.
+a=l[0:5]
+b=l[5:7]
+c=l[7:]
+
+
+#3.
+erg=c+a
+print(erg)
+d=erg[0::2]
+
+e=erg[1::2]
+
+
+
+#4.
+e=erg[::-1]
+
+
+#5.
+codewort=d+e+b
+print(codewort)
+print('Das Codewort ist '+ ''.join(codewort))
+
+
